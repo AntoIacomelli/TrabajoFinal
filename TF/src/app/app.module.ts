fd3679c0eebca0ce4b54cf6aa08af5d7dcc706bc
AntoIacomelli <95882662+AntoIacomelli@users.noreply.github.com>
2022-07-24 00:52:39 -0300

    Se dividió en componentes

--- TF/src/app/app.module.ts
+++ TF/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { LogoARComponent } from './components/logo-ar/logo-ar.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    LogoARComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
---
+++ TF/src/app/components/logo-ar/logo-ar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-ar',
+  templateUrl: './logo-ar.component.html',
+  styleUrls: ['./logo-ar.component.css']
+})
+export class LogoARComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
